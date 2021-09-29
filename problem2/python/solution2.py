59fd922ec8e8343f8c68d777f83496351c587b45
Andy <andy.kakkaramadam@gmail.com>
2021-09-29 00:52:26 -0400

    Add caching solution in python

--- problem2/python/solution2.py
+++ problem2/python/solution2.py
@@ -8,3 +8,27 @@ def productList(numbers):
         rhs = reduce(lambda total, val: total * val, numbers[i+1:], 1)
         ans.append(lhs * rhs)
     return ans
+
+    """[This is a cached approach that has 3 iterations per number]
+    """
+
+
+def productList2(numbers):
+    ans = []
+    lhs = [1]
+    rhs = [1]
+    lhsProd = 1
+    rhsProd = 1
+    for val in numbers[:-1]:
+        lhsProd *= val
+        lhs.append(lhsProd)
+    for val in numbers[:0:-1]:
+        rhsProd *= val
+        rhs.insert(0, rhsProd)
+    for i in range(len(numbers)):
+        ans.append(lhs[i] * rhs[i])
+    return ans
+
+
+if __name__ == '__main__':
+    print(productList2([2, 3, 5, 4]))
--- problem2/python/tests.py
+++ problem2/python/tests.py
@@ -1,11 +1,42 @@
-from solution2 import productList
+from solution2 import productList, productList2
 import unittest
 
 
-class TestAlgo2(unittest.TestCase):
+class TestAlgo2Reducer(unittest.TestCase):
 
     def __init__(self, methodName):
         self.algo = productList
+        super().__init__(methodName)
+
+    def test_1(self):
+        inputArr = [1]
+        result = [1]
+        self.assertListEqual(self.algo(inputArr), result)
+
+    def test_2(self):
+        inputArr = [1, 2]
+        result = [2, 1]
+        self.assertListEqual(self.algo(inputArr), result)
+
+    def test_3(self):
+        inputArr = [2, 1]
+        result = [1, 2]
+        self.assertListEqual(self.algo(inputArr), result)
+
+    def test_4(self):
+        inputArr = [4, 2, 1, 3]
+        result = [6, 12, 24, 8]
+        self.assertListEqual(self.algo(inputArr), result)
+
+    def test_5(self):
+        inputArr = [1, 1, 1, 1, 0, 1, 1]
+        result = [0, 0, 0, 0, 1, 0, 0]
+        self.assertListEqual(self.algo(inputArr), result)
+
+
+class TestAlgo2Cached(unittest.TestCase):
+    def __init__(self, methodName):
+        self.algo = productList2
         super().__init__(methodName)
 
     def test_1(self):
